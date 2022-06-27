592acfe9369b9e2460a1d9126b1f9ddb6193ab4c
Johannes Langosch <johannes.langosch@googlemail.com>
2022-06-27 13:21:35 +0200

    02 implementing authentification

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
-import Head from 'next/head'
+import Head from "next/head";
+
+import Heading from "components/Heading";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title></title>
-        <meta name='description' content='' />
-        <link rel='icon' href='/favicon.ico' />
+        <title>YouTube Clone</title>
+        <meta name="description" content="A great YouTube Clone" />
+        <title>Digital Downloads</title>
+        <meta name="description" content="Digital Downloads Website" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <h1>Welcome!</h1>
+      <Heading />
+
+      <h1 className="flex justify-center mt-20 text-xl">Welcome!</h1>
     </div>
-  )
+  );
 }
